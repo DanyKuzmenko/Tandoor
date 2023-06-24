9592fa81f808a8619438590d6940a8005877ec11
mark novikov <marknoviko@mail.ru>
2023-06-24 14:04:29 +0300

    kpi page visual part

--- src/pages/kpiPage/kpiPage.jsx
+++ src/pages/kpiPage/kpiPage.jsx
@@ -1,63 +1,60 @@
 import React from "react";
 import './kpiPage.scss';
+import KpiContent from "../../components/kpiContentComponent";
 
 const KpiPage = () => {
   const items = [
-    {name: 'Адель ВХ', percent: 99},
-    {name: 'Урбан ВХ', percent: 95},
-    {name: 'Доканто серый бархат ВХ', percent: 86},
-    {name: 'Сити зеркало Метал-Метал', percent: 75},
-    {name: 'Лу ВХ', percent: 69},
-    {name: 'Сицилия ВХ', percent: 58},
-    {name: 'Гладкая ВХ', percent: 45},
-    {name: 'Американская решетка ВХ', percent: 37},
-    {name: 'Барокко', percent: 26},
-    {name: 'Лиственница ВХ', percent: 20},
-    {name: 'Лиственница ВХ', percent: 18},
-    {name: 'Лиственница ВХ', percent: 15},
-    {name: 'Лиственница ВХ', percent: 12},
-    {name: 'Лиственница ВХ', percent: 5},
+    {name: 'Адель ВХ', percent: 99, interior: true},
+    {name: 'Урбан ВХ', percent: 95, interior: true},
+    {name: 'Доканто серый бархат ВХ', percent: 86, interior: true},
+    {name: 'Сити зеркало Метал-Метал', percent: 75, interior: true},
+    {name: 'Лу ВХ', percent: 69, interior: true},
+    {name: 'Сицилия ВХ', percent: 58, interior: true},
+    {name: 'Гладкая ВХ', percent: 45, interior: true},
+    {name: 'Американская решетка ВХ', percent: 37, interior: true},
+    {name: 'Барокко', percent: 26, interior: true},
+    {name: 'Лиственница ВХ', percent: 20, interior: true},
+    {name: 'Лиственница ВХ', percent: 18, interior: true},
+    {name: 'Лиственница ВХ', percent: 15, interior: true},
+    {name: 'Лиственница ВХ', percent: 12, interior: true},
+    {name: 'Лиственница ВХ', percent: 5, interior: true},
   ]
-  const countBarWidth = (item) => {
-    return item.percent * 970 / 100;
-  }
+  const selectOptions = [
+      "KPI №1",
+      "KPI №2",
+      "KPI №3",
+  ]
 
-  // setting opacity to the bars
-  for(let i = 0; i < items.length; i++) {
-    if(i === 0) {
-      items[i].opacity = 1;
-      continue;
-    }
-    items[i].opacity = items[i - 1].opacity <= 0.6 ? 0.5 : items[i - 1].opacity - 0.05;
-  }
   return (
     <div className="kpi-container">
       <header className="kpi-header">
-        <div className="kpi-header-inputs-container">
-          <input className="kpi-header__input" type="radio" id="kpi-view-1" name="kpi-view"/>
-          <label className="kpi-header__label" htmlFor="kpi-view-1"></label>
-          <input className="kpi-header__input" type="radio" id="kpi-view-2" name="kpi-view"/>
-          <label className="kpi-header__label" htmlFor="kpi-view-2"></label>
+        <div className="kpi-header-container">
+          <div className="kpi-header-inputs-container">
+            <input defaultChecked className="kpi-header-view__input" type="radio" id="kpi-view-1" name="kpi-view"/>
+            <label className="kpi-header-view__label" htmlFor="kpi-view-1"></label>
+            <input className="kpi-header-view__input" type="radio" id="kpi-view-2" name="kpi-view"/>
+            <label className="kpi-header-view__label" htmlFor="kpi-view-2"></label>
+          </div>
+          <h2 className="kpi-header_head">KPI помодельный</h2>
         </div>
-        <h2 className="kpi-header_head">KPI помодельный</h2>
+        <div className="kpi-header-filter-group">
+          <input defaultChecked type="radio" id="doors-type-1" name="doors-type" className="kpi-header-filter-group__radio"/>
+          <label className="kpi-header-filter-group__label" htmlFor="doors-type-1">Входные двери</label>
+          <input type="radio" id="doors-type-2" name="doors-type" className="kpi-header-filter-group__radio"/>
+          <label className="kpi-header-filter-group__label" htmlFor="doors-type-2">Межкомнатные двери</label>
+          <select className="kpi-header-filter-group-select" name="kpi-filter-select">
+            {selectOptions.map((selectOption) => (
+                <option className="kpi-header-filter-group-select__option" key={selectOption}>
+                  {selectOption}
+                </option>
+            ))}
+          </select>
+        </div>
       </header>
-      <div className="kpi-content">
-        <div className="kpi-content-names">
-          {items.map((item) => {
-            return <div key={item.name + item.percent} className="kpi-content-names__item">{item.name}</div>
-          })}
-        </div>
-        <div className="kpi-content-lines">
-          {items.map((item) => {
-            return (
-              <div key={item.name + item.percent} className="kpi-content-lines-line">
-                <span style={{width:countBarWidth(item) + "px", opacity: item.opacity}}  className="kpi-content-lines-line__bar"></span>
-                <span className="kpi-content-lines-line__percentage">{item.percent}%</span>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+
+      <KpiContent
+          items = {items}
+      />
     </div>
   )
 };
