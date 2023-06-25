ee8c3a728b49a574b99a29bdf2346d71dff07eff
unknown <marknoviko@mail.ru>
2023-06-25 20:19:46 +0300

    filtering by both kpi and door-type implemented

--- src/pages/kpiPage/kpiPage.jsx
+++ src/pages/kpiPage/kpiPage.jsx
@@ -1,53 +1,67 @@
-import React, {useState} from "react";
+import React, { useMemo, useState } from "react";
 import './kpiPage.scss';
 import KpiContent from "../../components/kpiContentComponent";
 import KpiHeader from "../../components/kpiHeaderComponent";
 
 const KpiPage = () => {
-    const items = [
-        {name: 'Адель ВХ', percent: 99, type: "entrance"},
-        {name: 'Урбан ВХ', percent: 95, type: "interior"},
-        {name: 'Доканто серый бархат ВХ', percent: 86, type: "interior"},
-        {name: 'Сити зеркало Метал-Метал', percent: 75, type: "entrance"},
-        {name: 'Лу ВХ', percent: 69, type: "interior"},
-        {name: 'Сицилия ВХ', percent: 58, type: "interior"},
-        {name: 'Гладкая ВХ', percent: 45, type: "interior"},
-        {name: 'Американская решетка ВХ', percent: 37, type: "interior"},
-        {name: 'Барокко', percent: 26, type: "entrance"},
-        {name: 'Лиственница ВХ', percent: 20, type: "interior"},
-        {name: 'Лиственница ВХ', percent: 18, type: "interior"},
-        {name: 'Лиственница ВХ', percent: 15, type: "interior"},
-        {name: 'Лиственница ВХ', percent: 12, type: "entrance"},
-        {name: 'Лиственница ВХ', percent: 5, type: "entrance"},
-    ]
-     const selectOptions = [
-        {value: "KPI1", label: "KPI № 1"},
-        {value: "KPI2", label: "KPI № 2"},
-        {value: "KPI3", label: "KPI № 3"}
-    ]
-    const [doors, setDoors] = useState(items);
-    const handleDoorsTypeRadio = (ev) => {
-        console.log(ev.target)
-        setDoors([...items].filter((door) => {
-            if (ev.target.value === "all") {
-                return true
-            }
-            return door.type === ev.target.value;
-        }))
-    }
+  const items = [
+    { name: 'Адель ВХ', percent: 99, type: "entrance", kpi: "KPI1" },
+    { name: 'Урбан ВХ', percent: 95, type: "interior", kpi: "KPI2" },
+    { name: 'Доканто серый бархат ВХ', percent: 86, type: "interior", kpi: "KPI3"},
+    { name: 'Сити зеркало Метал-Метал', percent: 75, type: "entrance", kpi: "KPI2"},
+    { name: 'Лу ВХ', percent: 69, type: "interior", kpi: "KPI1"},
+    { name: 'Сицилия ВХ', percent: 58, type: "interior", kpi: "KPI3"},
+    { name: 'Гладкая ВХ', percent: 45, type: "interior", kpi: "KPI2"},
+    { name: 'Американская решетка ВХ', percent: 37, type: "interior", kpi: "KPI3"},
+    { name: 'Барокко', percent: 26, type: "entrance", kpi: "KPI1"},
+    { name: 'Лиственница ВХ', percent: 20, type: "interior", kpi: "KPI3"},
+    { name: 'Лиственница ВХ', percent: 18, type: "interior", kpi: "KPI2"},
+    { name: 'Лиственница ВХ', percent: 15, type: "interior", kpi: "KPI3"},
+    { name: 'Лиственница ВХ', percent: 12, type: "entrance", kpi: "KPI1"},
+    { name: 'Лиственница ВХ', percent: 5, type: "entrance", kpi: "KPI2"},
+  ]
+  const selectOptions = [
+    { value: "", label: "Все KPI"},
+    { value: "KPI1", label: "KPI № 1" },
+    { value: "KPI2", label: "KPI № 2" },
+    { value: "KPI3", label: "KPI № 3" }
+  ]
+  const [doors, setDoors] = useState(items);
+  const [selectedOption, setSelectedOption] = useState();
+  const getKpiFilteredDoors = () => {
+    console.log("fsdfkk")
+    return selectedOption ? [...doors].filter((door) => door.kpi === selectedOption) : doors;
+  }
+  const kpiFilteredDoors = useMemo(
+    () => getKpiFilteredDoors(),
+    [doors, selectedOption])
+  
+    const handleSelectChange = (value) => {
+    setSelectedOption(value);
+  };
+  const handleDoorsTypeRadio = (ev) => {
+    setDoors([...items].filter((door) => {
+      if (ev.target.value === "all") {
+        return true
+      }
+      return door.type === ev.target.value;
+    }))
+  }
 
-    return (
-        <div className="kpi-container">
-            <KpiHeader
-                selectOptions={selectOptions}
-                filterByType={handleDoorsTypeRadio}
-            />
+  return (
+    <div className="kpi-container">
+      <KpiHeader
+        selectValue={selectedOption}
+        selectOptions={selectOptions}
+        filterByType={handleDoorsTypeRadio}
+        selectChange={handleSelectChange}
+      />
 
-            <KpiContent
-                items={doors}
-            />
-        </div>
-    )
+      <KpiContent
+        items={kpiFilteredDoors}
+      />
+    </div>
+  )
 };
 
 export default KpiPage;
