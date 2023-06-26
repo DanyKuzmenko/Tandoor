9b5bf21c3dc6e89e495d2bfb9126e45c0c989a5b
daniil-kuzmenko <daniil-kuzmenko@integlab.ru>
2023-06-26 14:41:29 +0300

    -Сделал блок поиска на главной странице

---
+++ src/components/SearchBlock/SearchBlock.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './SearchBlock.scss';
+import Input from '../Input/Input';
+import Select, { components } from 'react-select';
+import SearchResultBlock from './SearchResultBlock/SearchResultBlock';
+
+const options = [
+  {
+    value: 'Текущий KPI и мотивация',
+    label: 'Текущий KPI и мотивация',
+  },
+  {
+    value: 'Другой KPI',
+    label: 'Другой KPI',
+  },
+  {
+    value: 'Третий KPI',
+    label: 'Третий KPI',
+  },
+];
+
+const SearchBlock = () => {
+  const [selectOption, setSelectOption] = useState(options[0]);
+  const [isSearchResultBlockOpen, setIsSearchResultBlockOpen] = useState(false);
+
+  const handleSelectOption = (e) => {
+    setSelectOption(e);
+  };
+
+  const handleOpenSearchResultBlock = () => {
+    setIsSearchResultBlockOpen(!isSearchResultBlockOpen);
+  };
+
+  return (
+    <div className="search-block__wrapper">
+      <div className="search-block">
+        <Select
+          className="search-block__select"
+          classNamePrefix="search-block__select-prefix"
+          options={options}
+          value={selectOption}
+          onChange={(e) => handleSelectOption(e)}
+          unstyled={true}
+          components={{ DropdownIndicator }}
+          name="search-select"
+        />
+        <div className="search-block__container">
+          <Input placeholder={'Поиск задач...'} />
+          <button
+            className="search-block__button"
+            onClick={handleOpenSearchResultBlock}
+          >
+            Подробнее
+            <span className="search-block__button-right-arrow" />
+          </button>
+        </div>
+      </div>
+      {isSearchResultBlockOpen ? <SearchResultBlock /> : ''}
+    </div>
+  );
+};
+
+const DropdownIndicator = (props) => (
+  <components.DropdownIndicator {...props}>
+    <span className="search-block__select-dropdown-indicator" />
+  </components.DropdownIndicator>
+);
+
+export default SearchBlock;
--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -3,13 +3,16 @@ import Header from "../../components/Header/Header";
 import "./mainPage.scss";
 import PageAside from "../../components/PageAside/PageAside";
 import PageWrapper from "../../components/PageWrapper/pageWrapper";
+import SearchBlock from "../../components/SearchBlock/SearchBlock";
 
 const MainPage = () => {
   return (
     <PageWrapper>
       <PageAside />
       <Header />
-      <main></main>
+      <main className="main">
+        <SearchBlock />
+      </main>
     </PageWrapper>
   );
 };
