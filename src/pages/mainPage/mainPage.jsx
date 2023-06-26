089c832e95f70a0d22bbc3a4b1f605d48a71df5c
daniil-kuzmenko <daniil-kuzmenko@integlab.ru>
2023-06-26 12:03:14 +0300

    v2

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import "./mainPage.scss";
 import PageAside from "../../components/PageAside/PageAside";
+import PageWrapper from "../../components/PageWrapper/pageWrapper";
 
 const MainPage = () => {
   return (
-    <div className="main-page">
+    <PageWrapper>
       <PageAside />
       <Header />
-      <main></main>
-    </div>
+      <main>
+
+      </main>
+    </PageWrapper>
   );
 };
 
