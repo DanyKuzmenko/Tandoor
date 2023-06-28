4343f3ee693463d2090328347fabee09789129b2
Daniil Kuzmenko <daniil-kuzmenko@smartigy.ru>
2023-06-28 12:05:02 +0300

    v4

--- src/components/SearchBlock/SearchResultBlock/SearchResultBlock.jsx
+++ src/components/SearchBlock/SearchResultBlock/SearchResultBlock.jsx
@@ -1,81 +1,28 @@
 import React from 'react';
 import './SearchResultBlock.scss';
+import { tableData } from '../../../utils/constants';
 
 const SearchResultBlock = () => {
   return (
-    <div className="result-block">
-      <table className="result-block__table">
+    <div className='result-block'>
+      <table className='result-block__table'>
         <thead>
-          <tr>
-            <th className="result-block__thead-th" scope="col">
-              №
+        <tr>
+          {tableData.head.map((column) => (
+            <th className='result-block__thead-th' scope='col' key={column.id}>
+              {column.value}
             </th>
-            <th className="result-block__thead-th" scope="col">
-              Краткое наименование
-            </th>
-            <th className="result-block__thead-th" scope="col">
-              Полное наименование
-            </th>
-            <th className="result-block__thead-th" scope="col">
-              Содержание
-            </th>
-          </tr>
+          ))}
+        </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="result-block__tbody-td">001</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Июльский KPI по межкомнатным дверям
-            </td>
-            <td className="result-block__tbody-td">
-              Прага/Венге/Классика/Александра/Виктория
-            </td>
+        {tableData.body.map((bodyRow) => (
+          <tr key={bodyRow.id}>
+            {bodyRow.rowData.map((rowColumn) => (
+              <td className='result-block__tbody-td' key={rowColumn.id}>{rowColumn.value}</td>
+            ))}
           </tr>
-          <tr>
-            <td className="result-block__tbody-td">002</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Летная мотивация по входным дверям
-            </td>
-            <td className="result-block__tbody-td">Входные двери</td>
-          </tr>
-          <tr>
-            <td className="result-block__tbody-td">003</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Июльский KPI по межкомнатным дверям
-            </td>
-            <td className="result-block__tbody-td">Александра/Виктория</td>
-          </tr>
-          <tr>
-            <td className="result-block__tbody-td">004</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Продать складские остатки за премию
-            </td>
-            <td className="result-block__tbody-td">
-              Межкомнатные складские остатки
-            </td>
-          </tr>
-          <tr>
-            <td className="result-block__tbody-td">005</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Июльский KPI по межкомнатным дверям
-            </td>
-            <td className="result-block__tbody-td">
-              Прага/Венге/Классика/Александра/Виктория
-            </td>
-          </tr>
-          <tr>
-            <td className="result-block__tbody-td">006</td>
-            <td className="result-block__tbody-td">KPI №1</td>
-            <td className="result-block__tbody-td">
-              Летная мотивация по входным дверям
-            </td>
-            <td className="result-block__tbody-td">Фурнитура/Дверные ручки</td>
-          </tr>
+        ))}
         </tbody>
       </table>
     </div>
